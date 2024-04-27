eedfa2ec82bbdd62acb7058f3aae3fbaac9de7fc
iyatgithub <163440576+iyatgithub@users.noreply.github.com>
2024-04-27 17:28:10 +0500

    Create jquery-3.7.1.min.js

---
+++ jquery-3.7.1.min.js
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>JQuery Task</title>
+    <script src="./jquery-3.7.1.min.js"></script>
+    <script src="./jquery task.js"></script>
+    <link rel="stylesheet" href="./jquery task.css">
+</head>
+<body>
+    <input type="text" id="newtodo">
+    <button id="addbutton">Add To Do</button>
+    <ul id="todos"> 
+        <li>Milk</li>
+        <li>Banana</li>
+        <li>Tea</li>
+    </ul>
+</body>
+</html>
